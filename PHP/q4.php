5b6e09d4bb9b6536a8be617b31eded99da147288
Aditya Kumar <adityakr365@gmail.com>
2022-02-02 19:15:59 +0530

    adding

--- PHP/q4.php
+++ PHP/q4.php
@@ -9,6 +9,7 @@ if(isset($_POST['ans3'])){
     $password="";
     $dbname = "quiz";
     $con=mysqli_connect($server,$username,$password,$dbname);
+    $conn=mysqli_connect($server,$username,$password,$dbname);
     if(!$con){
         die("connection to this databassed faliled due to".mysqli_connect_error());
 
@@ -27,11 +28,33 @@ if(isset($_POST['ans3'])){
             // echo"Successfully inserted";
             $submit=1;
             
+            
     
         }
         else{
             echo"error: $sql <br> $con->error";
         }
+        $sql1="Select marks from leaderboard1 WHERE id='$ID'";
+        $result = $conn->query($sql1);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+              echo "id: " . $row["id"];
+            }
+          } else {
+            echo "0 results";
+          }
+        // if($con->query($sql)== true)
+        // {
+        //     // echo"Successfully inserted";
+        //     $submit=1;
+            
+            
+    
+        // }
+        // else{
+        //     echo"error: $sql <br> $con->error";
+        // }
         $con->close();
        
     }
